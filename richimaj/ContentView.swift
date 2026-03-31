84b8aecdbd07d7b0152aeb74ab3999146510cd56
zihaoLin-1107 <zhlin.ee06@nycu.edu.tw>
2026-03-31 19:24:00 +0800

    Initial Commit

---
+++ richimaj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  richimaj
+//
+//  Created by linzihao on 2026/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ richimaj/richimajApp.swift
@@ -0,0 +1,17 @@
+//
+//  richimajApp.swift
+//  richimaj
+//
+//  Created by linzihao on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct richimajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
